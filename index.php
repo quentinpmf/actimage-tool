b1ba03f7e606016d9cd99645da5199701bfa81a8
Quentin Boudinot <quentin.boudinot@actimage.com>
2019-04-30 09:38:11 +0200

    remove projects order by name ASC car pb avec liste déroulante

--- index.php
+++ index.php
@@ -161,7 +161,7 @@ if(!isset($_SESSION['UserEmail']))
                                                 <?php
                                                 $j = 1; //nb de projets dans la liste
 
-                                                $req2 = $bdd->query("SELECT * FROM projects ORDER BY projects.name ASC");
+                                                $req2 = $bdd->query("SELECT * FROM projects");
                                                 while ($projects = $req2->fetch())
                                                 {
                                                     //selection du projet dans la liste
